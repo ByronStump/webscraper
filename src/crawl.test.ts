6ec5b97851806611dd6c1fdd7142080eca834bf6
Byron Stump <jellyeater343@gmail.com>
2026-02-05 12:11:04 -0800

    added some html extraction functions for h1 and p tags. Updated tests as well.

--- src/crawl.test.ts
+++ src/crawl.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { normalizeURL } from "./crawl";
+import { getFirstParagraphFromHTML, getH1FromHTML, normalizeURL } from "./crawl";
 
 test('normalizes input url from: https://blog.boot.dev/path/ to: blog.boot.dev/path', () => {
     expect(normalizeURL("https://blog.boot.dev/path/")).toBe("blog.boot.dev/path")
@@ -13,3 +13,57 @@ test('normalizes input url from: https://blog.boot.dev to: blog.boot.dev', () =>
 test('normalizes input url from: https://blog.boot.dev/path/aaaa/bbbb/cccc to: blog.boot.dev/path/aaaa/bbbb/cccc', () => {
     expect(normalizeURL("https://blog.boot.dev/path/aaaa/bbbb/cccc")).toBe("blog.boot.dev/path/aaaa/bbbb/cccc")
 })
+
+test("getH1FromHTML basic", () => {
+  const inputBody = `<html><body><h1>Test Title</h1></body></html>`;
+  const actual = getH1FromHTML(inputBody);
+  const expected = "Test Title";
+  expect(actual).toEqual(expected);
+});
+test("getH1FromHTML blank", () => {
+  const inputBody = `<html><body><p>something something</p><h2>HI</h2></body></html>`;
+  const actual = getH1FromHTML(inputBody);
+  const expected = "";
+  expect(actual).toEqual(expected);
+});
+
+
+test("getFirstParagraphFromHTML main priority", () => {
+  const inputBody = `
+    <html><body>
+      <p>Outside paragraph.</p>
+      <main>
+        <p>Main paragraph.</p>
+      </main>
+    </body></html>
+  `;
+  const actual = getFirstParagraphFromHTML(inputBody);
+  const expected = "Main paragraph.";
+  expect(actual).toEqual(expected);
+});
+test("getFirstParagraphFromHTML blank", () => {
+  const inputBody = `
+    <html><body>
+      <h1>something</h1>
+      <main>
+        <h2>Main paragraph.</h2>
+      </main>
+    </body></html>
+  `;
+  const actual = getFirstParagraphFromHTML(inputBody);
+  const expected = "";
+  expect(actual).toEqual(expected);
+});
+test("getFirstParagraphFromHTML only outside p", () => {
+  const inputBody = `
+    <html><body>
+      <p>Outside paragraph.</p>
+      <main>
+        <h2>Main h2.</h2>
+      </main>
+    </body></html>
+  `;
+  const actual = getFirstParagraphFromHTML(inputBody);
+  const expected = "Outside paragraph.";
+  expect(actual).toEqual(expected);
+});
--- src/crawl.ts
+++ src/crawl.ts
@@ -1,4 +1,5 @@
 import url from "node:url";
+import { JSDOM } from "jsdom";
 
 export function normalizeURL(rawURL: string): string {
     const parsedURL = url.parse(rawURL)
@@ -11,4 +12,19 @@ export function normalizeURL(rawURL: string): string {
 
     }
     return parsedURL.host + parsedURL.path.replace(/\/$/, "")
+}
+
+export function getH1FromHTML(html: string): string{
+    const dom = new JSDOM(html)
+    const h1 = dom.window.document.querySelector("h1")
+
+    return h1 ? h1.innerHTML : ""
+}
+
+export function getFirstParagraphFromHTML(html: string): string{
+    const dom = new JSDOM(html)
+    const doc = dom.window.document
+    const main = doc.querySelector("main")
+    const p = main?.querySelector("p") ?? doc.querySelector("p")
+    return p ? p.innerHTML : ""
 }
